094bdff78018802b7b8d6e55cf187a48995f410c
Maria <mariamoussamoussa96@gmail.com>
2021-04-03 12:11:58 +0300

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('ok')
+app.get("/", (req, res) => {
+  res.send("ok");
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get("/test", (req, res) => {
+    res.send({status: 200, message:"ok"});
+})
+
+app.get("/time", (req, res) => {
+    let time=new Date(Date.now());
+    res.send({status: 200, message:time.getHours()+":"+time.getMinutes()});
+});
