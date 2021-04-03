c175efecfc999b58354e7438bb3dc496d60c62d6
Maria <mariamoussamoussa96@gmail.com>
2021-04-03 12:17:06 +0300

    step 4

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("ok");
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get("/", (req, res) => {
+    res.send("ok");
 })
 
 app.get("/test", (req, res) => {
-    res.send({status: 200, message:"ok"});
+    res.send({ status: 200, message: "ok" });
 })
 
 app.get("/time", (req, res) => {
-    let time=new Date(Date.now());
-    res.send({status: 200, message:time.getHours()+":"+time.getMinutes()});
+    let time = new Date(Date.now());
+    res.send({ status: 200, message: time.getHours() + ":" + time.getMinutes() });
+});
+
+app.get("/hello/:id", (req, res) => {
+    res.send("hello " + req.params.id);
+});
+
+app.get("/search", (req, res) => {
+    if (!req.query["s"]) {
+        res.send({
+            status: 500,
+            error: true,
+            message: "you have to provide a search",
+        });
+    } else {
+        res.send({ status: 200, message: "ok", data: req.query["s"] });
+    }
 });
