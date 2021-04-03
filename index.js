c40cbb07a37bd253905b37075e0113db52ca3de5
Maria <mariamoussamoussa96@gmail.com>
2021-04-03 12:38:57 +0300

    step 5

--- index.js
+++ index.js
@@ -25,12 +25,44 @@ app.get("/hello/:id", (req, res) => {
 
 app.get("/search", (req, res) => {
     if (!req.query["s"]) {
-        res.send({
-            status: 500,
-            error: true,
-            message: "you have to provide a search",
-        });
+        res.send({ status: 500, error: true, message: "you have to provide a search" });
     } else {
         res.send({ status: 200, message: "ok", data: req.query["s"] });
+    }
+});
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get("/movies/add", (req, res) => {
+    res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/get", (req, res) => {
+    res.send({ status: 200, data: movies });
+});
+
+app.get("/movies/edit", (req, res) => {
+    res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/delete", (req, res) => {
+    res.send({ status: 200, message: "ok" });
+});
+
+app.get("/movies/read/id/:id", (req, res) => {
+    let id = req.params.id;
+    if (Object.values(movies)[id - 1] != null) {
+        res.send({ status: 200, data: Object.values(movies)[id - 1] });
+    } else {
+        res.send({
+            status: 404,
+            error: true,
+            message: "the movie " + id + " does not exist"
+        });
     }
 });
